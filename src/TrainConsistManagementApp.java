983aceb1bb6adee739fc1b4781f82623d8501a71
Siva-1409 <kk5266@srmist.edu.in>
2026-03-24 10:52:11 +0530

    Base application setup

---
+++ src/TrainConsistManagementApp.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class TrainConsistManagementApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Train Consist Management App ===");
+
+        List<String> bogies = new ArrayList<>();
+
+        System.out.println("Initial Bogie Count: " + bogies.size());
+    }
+}
