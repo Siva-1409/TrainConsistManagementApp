e1309344a192a92da1f2453ec8efacd0689794d1
Siva-1409 <kk5266@srmist.edu.in>
2026-04-16 23:57:27 +0530

    UC11:Validate Train ID & Cargo Codes (Regex)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,26 +1,15 @@
-import java.util.*;
+import java.util.regex.*;
 
 public class TrainConsistManagementApp {
 
-    static class Bogie {
-        int capacity;
-
-        Bogie(int capacity) {
-            this.capacity = capacity;
-        }
-    }
-
     public static void main(String[] args) {
-        List<Bogie> list = Arrays.asList(
-                new Bogie(72),
-                new Bogie(56),
-                new Bogie(24)
-        );
+        String trainId = "TRN-1234";
+        String cargo = "CRG-567";
 
-        int total = list.stream()
-                .map(b -> b.capacity)
-                .reduce(0, Integer::sum);
+        boolean trainValid = Pattern.matches("TRN-\\d{4}", trainId);
+        boolean cargoValid = Pattern.matches("CRG-\\d{3}", cargo);
 
-        System.out.println("Total Seats: " + total);
+        System.out.println(trainValid);
+        System.out.println(cargoValid);
     }
 }
