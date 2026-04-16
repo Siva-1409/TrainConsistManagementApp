0851a1d2afe615ea910c22d43bf12e79d2204022
Siva-1409 <kk5266@srmist.edu.in>
2026-04-16 23:59:46 +0530

    UC17:Sort Bogie Names Using Arrays.sort()

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -3,17 +3,9 @@ import java.util.*;
 public class TrainConsistManagementApp {
 
     public static void main(String[] args) {
-        int[] arr = {72, 56, 24};
+        String[] arr = {"Sleeper", "AC Chair", "First Class"};
 
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int t = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = t;
-                }
-            }
-        }
+        Arrays.sort(arr);
 
         System.out.println(Arrays.toString(arr));
     }
